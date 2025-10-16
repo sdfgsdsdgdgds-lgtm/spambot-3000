d8159bcf58fa905b84c6a1a57ddec498e2d6130c
sdfgsdsdgdgds-lgtm <sdfgsdsdgdgds@gmail.com>
2025-10-16 23:44:24 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+import asyncio
+import os
+
+# Hämta token och kanal-ID från miljövariabler
+TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Inloggad som {client.user}")
+    channel = client.get_channel(CHANNEL_ID)
+
+    # Loopa och skicka meddelande varje sekund
+    while True:
+        await channel.send("🔁 Meddelande varje sekund!")
+        await asyncio.sleep(1)  # Vänta 1 sekund
+
+client.run(TOKEN)
